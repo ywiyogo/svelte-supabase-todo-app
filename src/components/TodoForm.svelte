d3e5bf0c9a8ad327f9cbcc780b983f60b5fc1a09
Yongkie Wiyogo <6004558-ywiyogo@users.noreply.gitlab.com>
2022-06-03 10:21:03 +0200

    Create form component

---
+++ src/components/TodoForm.svelte
@@ -0,0 +1,26 @@
+<!-- TodoForm component describes the form to input a todo entry -->
+<script lang="ts">
+  let todo = "";
+  const handleSubmit = () => {
+    todo = "";
+    console.log("submitting");
+  };
+</script>
+
+<form action="" class="my-6" on:submit|preventDefault={handleSubmit}>
+  <div class="flex flex-col mb-2">
+    <label for="todo" class="font-bold mb-2 text-gray-800">Todo</label>
+    <input
+      type="text"
+      name="todo"
+      bind:value={todo}
+      placeholder="what's app"
+      class="border p-2 shadow-sm rounded-lg border-gray-200 focus:outline-none focus:border-gray-500 "
+    />
+    <button
+      type="submit"
+      class="border w-full py-2 px-4 shadow-sm  text-white bg-blue-500 hover:bg-blue-700"
+      >Submit</button
+    >
+  </div>
+</form>
--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,2 +1,10 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script>
+  import TodoForm from "../components/TodoForm.svelte";
+</script>
+
+<main>
+  <h1 class="text-2xl text-center font-bold text-gray-800 md:text-3xl">
+    My First Svelte Todo App
+  </h1>
+  <TodoForm />
+</main>
