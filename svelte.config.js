8dc11cca5d5a9cab80a767e30e7e9fa47376657d
Yongkie Wiyogo <6004558-ywiyogo@users.noreply.gitlab.com>
2022-06-03 10:54:42 +0200

    Configure Github Pages setting

--- svelte.config.js
+++ svelte.config.js
@@ -1,15 +1,22 @@
-import adapter from '@sveltejs/adapter-auto';
-import preprocess from 'svelte-preprocess';
+// import adapter from '@sveltejs/adapter-auto';	// default
+import adapter from "@sveltejs/adapter-static"; // for static page
+import preprocess from "svelte-preprocess";
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://github.com/sveltejs/svelte-preprocess
-	// for more information about preprocessors
-	preprocess: preprocess(),
+  // Consult https://github.com/sveltejs/svelte-preprocess
+  // for more information about preprocessors
+  preprocess: preprocess(),
 
-	kit: {
-		adapter: adapter()
-	}
+  kit: {
+    adapter: adapter(),
+    prerender: {
+      default: true,
+    },
+    paths: {
+		base: process.env.NODE_ENV === "production" ? "/svelte-supabase-todo-app" : "",
+	},
+  },
 };
 
 export default config;
